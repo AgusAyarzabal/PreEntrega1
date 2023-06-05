aafedb01e13ed28cb7730754eaf69caaed04032d
AgusAyarzabal <127358474+AgusAyarzabal@users.noreply.github.com>
2023-06-05 16:20:19 -0300

    cambios definitivos realizados

--- index.js
+++ index.js
@@ -1,93 +1,73 @@
-//INGRESO DE DATOS PARA PODER UTILIZAR CALCULADORA
-//muestra por pantalla
-let nombreCompleto = prompt("Ingrese su nombre completo para continuar.")
-if (nombreCompleto == "") {
+let nombre = prompt("Ingrese su nombre para continuar.")
+if (nombre == "") {
     alert = ("Ingrese su nombre.")
 } else {
-    alert("Bienvenido " + nombreCompleto + ".")
+    alert("Bienvenido " + nombre + ".")
+}
+alert("Presione Aceptar para comenzar a utilizar la Calculadora.")
+
+// YA PUEDE UTILIZAR CALCULADORA
+
+// Función para sumar dos números
+function sumar(num1, num2) {
+    return num1 + num2;
 }
 
-let edad = parseInt(prompt("Debes ser mayor de edad para utilizar nuestros servicios. Ingrese su edad por favor."))
-if (edad >= 18) {
-    alert("Eres mayor, puedes continuar.")
-} else {
-    alert("No eres mayor.")
+// Función para restar dos números
+function restar(num1, num2) {
+    return num1 - num2;
 }
 
-if ((nombreCompleto != "") && (edad >= 18)) {
-    alert("Toque la tecla F12 y luego presione Aceptar para comenzar a utilizar la Calculadora.")
+// Función para multiplicar dos números
+function multiplicar(num1, num2) {
+    return num1 * num2;
+}
 
-    // YA PUEDE UTILIZAR CALCULADORA
-    // muestra por consola
+// Función para dividir dos números
+function dividir(num1, num2) {
+    return num1 / num2;
+}
 
-    // Función para sumar dos números
-    function sumar(num1, num2) {
-        return num1 + num2;
-    }
+// Función principal de la calculadora
+function calculadora() {
+    let opcion = "";
+    let num1, num2;
 
-    // Función para restar dos números
-    function restar(num1, num2) {
-        return num1 - num2;
-    }
+    while (opcion !== "0") {
+        opcion = prompt("Ingrese una opción: (1. para Sumar- 2. para Restar- 3. para Multiplicar- 4. para Dividir- 0 para Salir)");
 
-    // Función para multiplicar dos números
-    function multiplicar(num1, num2) {
-        return num1 * num2;
-    }
-
-    // Función para dividir dos números
-    function dividir(num1, num2) {
-        return num1 / num2;
-    }
-
-    // Función principal de la calculadora
-    function calculadora() {
-        let opcion = "";
-        let num1, num2;
-
-        while (opcion !== "0") {
-            console.log("Calculadora");
-            console.log("1. Sumar");
-            console.log("2. Restar");
-            console.log("3. Multiplicar");
-            console.log("4. Dividir");
-            console.log("0. Salir");
-
-
-            opcion = prompt("Ingrese una opción: ");
-
-            switch (opcion) {
-                case "1":
-                    num1 = parseInt(prompt("Ingrese el primer número: "));
-                    num2 = parseInt(prompt("Ingrese el segundo número: "));
-                    alert("Resultado: " + sumar(num1, num2));
-                    break;
-                case "2":
-                    num1 = parseInt(prompt("Ingrese el primer número: "));
-                    num2 = parseInt(prompt("Ingrese el segundo número: "));
-                    alert("Resultado: " + restar(num1, num2));
-                    break;
-                case "3":
-                    num1 = parseInt(prompt("Ingrese el primer número: "));
-                    num2 = parseInt(prompt("Ingrese el segundo número: "));
-                    alert("Resultado: " + multiplicar(num1, num2));
-                    break;
-                case "4":
-                    num1 = parseInt(prompt("Ingrese el primer número: "));
-                    num2 = parseInt(prompt("Ingrese el segundo número: "));
-                    alert("Resultado: " + dividir(num1, num2));
-                    break;
-                case "0":
-                    alert("Saliendo de la calculadora...");
-                    break;
-                default:
-                    alert("Opciòn no valida.");
-            }
+        switch (opcion) {
+            case "1":
+                alert("Usted presionò la opcion 1 para Sumar")
+                num1 = parseInt(prompt("Ingrese el primer número: "));
+                num2 = parseInt(prompt("Ingrese el segundo número: "));
+                alert("Resultado: " + sumar(num1, num2));
+                break;
+            case "2":
+                alert("Usted presionò la opcion 2 para Restar")
+                num1 = parseInt(prompt("Ingrese el primer número: "));
+                num2 = parseInt(prompt("Ingrese el segundo número: "));
+                alert("Resultado: " + restar(num1, num2));
+                break;
+            case "3":
+                alert("Usted presionò la opcion 3 para Multiplicar")
+                num1 = parseInt(prompt("Ingrese el primer número: "));
+                num2 = parseInt(prompt("Ingrese el segundo número: "));
+                alert("Resultado: " + multiplicar(num1, num2));
+                break;
+            case "4":
+                alert("Usted presionò la opcion 4 para Dividir")
+                num1 = parseInt(prompt("Ingrese el primer número: "));
+                num2 = parseInt(prompt("Ingrese el segundo número: "));
+                alert("Resultado: " + dividir(num1, num2));
+                break;
+            case "0":
+                alert("Saliendo de la calculadora...");
+                break;
+            default:
+                alert("Opciòn no valida.");
         }
-
     }
-} else {
-    alert("Los datos ingresados no son vàlidos.")
 
 }
 
